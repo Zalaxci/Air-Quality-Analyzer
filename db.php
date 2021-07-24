514379842fd83f58c71e2d7996e7ca37fd59c2ba
bitnimal <ima@cyber-wizard.com>
2021-07-24 11:24:11 +0300

    Decode json as array so medoo works

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
     require 'vendor/autoload.php';
     use Medoo\Medoo;
 
-    $db_config = json_decode(file_get_contents("db.json"));
+    $db_config = json_decode(file_get_contents("db.json"), true);
     $database = new Medoo($db_config);
 
 ?>
