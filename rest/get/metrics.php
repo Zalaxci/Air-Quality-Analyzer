225573e8f0569824a1cb09c2473bf906a7b72b43
bitnimal <ima@cyber-wizard.com>
2021-07-24 13:01:46 +0300

    Fixed db_config & metrics api issue for good

--- rest/get/metrics.php
+++ rest/get/metrics.php
@@ -10,7 +10,11 @@
     foreach ($schools as &$school) {
         $school['metrics'] = $database->select('metrics', [
             'm_id',
-            'sch_name'
+            'pm2_5',
+            'pm10',
+            'temp',
+            'humidity',
+            'timestamp'
         ], [
             'sch_id' => $school['sch_id']
         ]);
