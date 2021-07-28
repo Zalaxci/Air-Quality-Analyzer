6a0d1488a43db391ba54cd2da85149a5a7ec2227
bitnimal <ima@cyber-wizard.com>
2021-07-28 16:11:36 +0300

    Fixed cors error in get apis (hopefully)

--- rest/get/metrics.php
+++ rest/get/metrics.php
@@ -2,6 +2,9 @@
 
     require '../../db.php';
 
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET');
+
     $schools = $database->select('schools', [
         'sch_id',
         'sch_name'
