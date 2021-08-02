9eab11fc1e84d3f73ffc5b49ec6322ef1208b188
bitnimal <ima@cyber-wizard.com>
2021-08-02 17:40:25 +0300

    Use c3 because chartjs side labels are broken -_-

--- schools/components/chart.js
+++ schools/components/chart.js
@@ -1,25 +1,34 @@
-export function setChartColor(color) {
-    Chart.defaults.color = color;
+function getUniqueDates(schools) {
+    let allMetrics = schools.map(school => school.metrics).flat();
+    let allDates = allMetrics.map(metric => metric.date);
+    return [...new Set(allDates)];
 }
-export function drawChart(canvas, schools) {
-    let metrics = schools.map(school => school.metrics).flat();
-    new Chart(
-        canvas,
-        {
-            type: 'line',
-            data: {
-                labels: metrics.map(metric => metric.timestamp),
-                datasets: schools.map(school => {
-                    let label = school.sch_name;
-                    let [ red, green, blue ] = [ 200 / school.sch_id, 50 * school.sch_id, 255 ];
-                    let borderColor = `rgb(${red}, ${green}, ${blue})`;
-                    let data = school.metrics.map(metric => metric[canvas.id]);
-                    return { label, borderColor, data };
-                }),
-            },
-            options: {
-                maintainAspectRatio: false,
-            },
-        }
-    );
+function getSchoolMetrics(school, datatype) {
+    let metrics = school.metrics;
+    return metrics.map(metric => parseInt(metric[datatype]));
+}
+export function drawChart(container, schools) {
+    let columns = [
+        ['x', ...getUniqueDates(schools)]
+    ]
+    schools.forEach(school => {
+        let name = school.sch_name;
+        let metrics = getSchoolMetrics(school, container.id);
+        columns.push([name, ...metrics])
+    });
+    let chart = c3.generate({
+      bindto: container,
+      data: {
+        x: 'x',
+        columns
+      },
+      axis: {
+          x: {
+              type: 'timeseries',
+              tick: {
+                  format: '%Y-%m-%d'
+              }
+          }
+      }
+    });
 }
--- schools/data/render.js
+++ schools/data/render.js
@@ -1,11 +1,10 @@
 import { renderMap } from '../components/map.js';
-import { setChartColor, drawChart } from '../components/chart.js';
+import { drawChart } from '../components/chart.js';
 
 async function renderData(url) {
     let response = await fetch(url);
-    let data = await response.json();
-    setChartColor('#fff');
-    forElement('.map', renderMap, await data);
-    forElement('.chart', drawChart, await data);
+    let schools = await response.json();
+    forElement('.map', renderMap, await schools);
+    forElement('.c3', drawChart, await schools);
 }
 renderData('http://lessons.idrogios.com/air-quality-analyzer/Air-Quality-Analyzer/Air-Quality-Analyzer/rest/get/schools.php');
