0cd105cab3b85fbfa654c2eccba27736357f98d0
Παντελής Γκανάτσας <ima@cyber-wizard.com>
2021-08-24 22:45:22 +0300

    Fixed db directory issue

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
     require 'vendor/autoload.php';
     use Medoo\Medoo;
 
-    $db_config = json_decode(file_get_contents("/home/lessons/www/air-quality-analyzer/Air-Quality-Analyzer/Air-Quality-Analyzer/db.json"), true);
+    $db_config = json_decode(file_get_contents("/home/lessons/www/Air-Quality-Analyzer/Air-Quality-Analyzer/db.json"), true);
     $database = new Medoo($db_config);
 
 ?>
